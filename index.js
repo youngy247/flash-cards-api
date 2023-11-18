0440b765249f0eb4c5844ed9566bc332cd13c093
Adam Young <adamyoungy678@gmail.com>
2023-11-18 23:42:13 +0000

    feat: added routes to index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const connectDB = require('./src/config/db');
+const express = require("express");
+const connectDB = require("./src/config/db");
+const flashcardRoutes = require("./src/api/routes/flashcardRoutes");
 const app = express();
 app.use(express.json());
 
@@ -8,10 +9,7 @@ const port = 3000;
 // Connect to Database
 connectDB();
 
-// Placeholder route
-app.get('/', (req, res) => {
-  res.send('Flashcard Learning API');
-});
+app.use("/api/flashcards", flashcardRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
